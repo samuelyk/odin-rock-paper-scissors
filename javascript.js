5d8f7f2fc137e283b877079dac51faba9fbcdd59
samuelyk <youngjoong.s.kim@gmail.com>
2025-09-17 21:32:06 -0700

    Write getComputerChoice() function
    getComputerChoice() function returns "rock", "paper", or "scissors"
    based on a random number generated by Math.random()

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice () {
+    const randomNumber = Math.random();
+    if (randomNumber <= 1/3) {
+        console.log(randomNumber);
+        console.log("rock");
+        // return "rock";
+    } else if (randomNumber > 1/3 && randomNumber <= 2/3) {
+        console.log(randomNumber);
+        console.log("paper");
+        // return "paper";
+    } else {
+        console.log(randomNumber);
+        console.log("scissors");
+        // return "scissors";
+    }
+}
