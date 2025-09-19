6e8a60f8cfc5f2c564a9c7cc00c827f5cba16e38
samuelyk <youngjoong.s.kim@gmail.com>
2025-09-18 17:18:11 -0700

    Add a function playRound() for a single round game

--- javascript.js
+++ javascript.js
@@ -1,21 +1,72 @@
-function getComputerChoice () {
+function getComputerChoice() {
     const randomNumber = Math.random();
     if (randomNumber <= 1/3) {
-        console.log(randomNumber);
-        console.log("rock");
-        // return "rock";
+        return "rock";
     } else if (randomNumber > 1/3 && randomNumber <= 2/3) {
-        console.log(randomNumber);
-        console.log("paper");
-        // return "paper";
+        return "paper";
     } else {
-        console.log(randomNumber);
-        console.log("scissors");
-        // return "scissors";
+        return "scissors";
     }
 }
 
-function getHumanChoice () {
-    const userChoice = prompt("Choose one of \"Rock\", \"Paper\", \"Scissors\": ");
+function getHumanChoice() {
+    const userChoice = 
+    prompt("Choose one of \"Rock\", \"Paper\", \"Scissors\": ");
     return userChoice;
 }
+
+let humanScore = 0, computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === "rock") { 
+        switch (computerChoice) {
+            case "rock":
+                console.log("No winner! Let's play again.");
+                break;
+            case "paper":
+                console.log("Computer wins!");
+                computerScore++;
+                break;
+            case "scissors":
+                console.log("You win!");
+                humanScore++;
+                break;
+        }
+    } else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You win!");
+                humanScore++;
+                break;
+            case "paper":
+                console.log("No winner! Let's play again.");
+                break;
+            case "scissors":
+                console.log("Computer wins!");
+                computerScore++;
+                break;  
+        }
+    } else if (humanChoice === "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("Computer wins!");
+                computerScore++;
+                break;
+            case "paper":
+                console.log("You win!");
+                humanScore++;
+                break;
+            case "scissors":
+                console.log("No winner! Let's play again.");
+                break;  
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
